c44625eee3d10cc674bde2a850ab4efd1c9a4848
sbx_user1051 <sbx_user1051@169.254.153.45>
2021-12-02 00:10:55 +0000

    Update requested for Proton - service/tf5-svc/service-instance/my-other-instance

---
+++ tf-env/tf5-svc-my-other-instance/proton.service_instance.variables.tf
@@ -0,0 +1,30 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:ap-northeast-1:259837135622:service/tf5-svc/service-instance/my-other-instance
+
+If the resource is no longer is accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+variable "environment" {
+  type = object({
+    account_id = string
+    name = string
+    outputs = map(string)
+  })
+  default = null
+}
+
+variable "service" {
+  type = object({
+    name = string
+  })
+}
+
+variable "service_instance" {
+  type = object({
+    name = string
+    inputs = map(string)
+  })
+  default = null
+}
