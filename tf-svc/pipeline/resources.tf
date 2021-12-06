9225e13f1c02c96155cfa136ab24ce791d1962a1
sbx_user1051 <sbx_user1051@169.254.91.93>
2021-12-06 17:56:39 +0000

    Update requested for Proton - service/tf-svc/pipeline

--- tf-svc/pipeline/resources.tf
+++ tf-svc/pipeline/resources.tf
@@ -1,7 +1,7 @@
 /*
 This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
 
-To manage this resource, see AWS Proton Resource: arn:aws:proton:ap-northeast-1:259837135622:service/tf-svc/pipeline
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-1:259837135622:service/tf-svc/pipeline
 
 If the resource is no longer is accessible within AWS Proton, it may have been deleted and may require manual cleanup.
 */
