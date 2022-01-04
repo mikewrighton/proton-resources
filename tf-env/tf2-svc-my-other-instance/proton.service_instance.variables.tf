6c2e7f53ae63b93f793ec7fbca2330aab9edfefa
sbx_user1051 <sbx_user1051@169.254.33.241>
2022-01-04 14:51:19 +0000

    Update requested for Proton - service/tf2-svc/service-instance/my-other-instance

--- tf-env/tf2-svc-my-other-instance/proton.service_instance.variables.tf
+++ tf-env/tf2-svc-my-other-instance/proton.service_instance.variables.tf
@@ -1,7 +1,7 @@
 /*
 This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
 
-To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-1:259837135622:service/tf2-svc/service-instance/my-other-instance
+To manage this resource, see AWS Proton Resource: arn:aws:proton:ap-northeast-1:259837135622:service/tf2-svc/service-instance/my-other-instance
 
 If the resource is no longer is accessible within AWS Proton, it may have been deleted and may require manual cleanup.
 */
@@ -28,3 +28,4 @@ variable "service_instance" {
   })
   default = null
 }
+
