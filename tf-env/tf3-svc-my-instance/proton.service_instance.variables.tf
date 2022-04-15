95e50d793873f03ae6d119650f8bea57ada44c9a
sbx_user1051 <sbx_user1051@169.254.228.233>
2022-04-15 18:10:16 +0000

    Update requested for Proton - service/tf3-svc/service-instance/my-instance

--- tf-env/tf3-svc-my-instance/proton.service_instance.variables.tf
+++ tf-env/tf3-svc-my-instance/proton.service_instance.variables.tf
@@ -1,31 +1,30 @@
 /*
-This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
-
-To manage this resource, see AWS Proton Resource: arn:aws:proton:ap-northeast-1:259837135622:service/tf3-svc/service-instance/my-instance
-
-If the resource is no longer is accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
 */
 
 variable "environment" {
   type = object({
     account_id = string
-    name = string
-    outputs = map(string)
+    name       = string
+    outputs    = map(string)
   })
   default = null
 }
 
 variable "service" {
   type = object({
-    name = string
+    name                      = string
+    branch_name               = string
+    repository_connection_arn = string
+    repository_id             = string
   })
 }
 
 variable "service_instance" {
   type = object({
-    name = string
-    inputs = map(string)
+    name       = string
+    inputs     = map(string)
+    components = map(string)
   })
   default = null
 }
-
